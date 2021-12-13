417e41558cb19367633b28bff6df525c5f63de05
Marcos Selin <marcosselinm@gmail.com>
2021-12-13 11:42:47 -0400

    env configurated

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser')
 mongoose.set('useCreateIndex', true)
 // mongoose.connect('mongodb://localhost:27017/node_auth', {
-mongoose.connect('mongodb+srv://markselin:MarcoS_1683947_@cluster0.uaau3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+// mongoose.connect('mongodb+srv://markselin:MarcoS_1683947_@cluster0.uaau3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect('process.env.CONNECT_MONGO', {
     
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -27,5 +28,5 @@ app.use(cors({
 app.use(express.json())
 
 app.use('/api', routes)
-
+//de esta forma cuando se ejecute localmente 8000 sera el puerto y tambien funcionara en heroku al hacer deploy
 app.listen(process.env.PORT || 8000)
